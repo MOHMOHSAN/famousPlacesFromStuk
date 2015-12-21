a1bd913315b338b169a29c58fa00a0411eb30a9f
Moh Moh San <violalwin@gmail.com>
2015-12-21 11:56:33 +0800

    added registration controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,11 @@
+class RegistrationsController < Devise::RegistraitonsController
+	private
+
+	def sign_up_params
+		params.require(:user).permit(:name,:email,:password)
+	end
+
+	def account_update_params
+		params.require(:user).permit(:name,:email,:password,:current_password)
+	end
+end
