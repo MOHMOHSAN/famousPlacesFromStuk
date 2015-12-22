c99f5e603f7ad5b3b5c4a6e789a1457776b4ae04
Moh Moh San <violalwin@gmail.com>
2015-12-22 18:25:51 +0800

    styling landing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
-  def home
-  end
+	before_action :authenticate_user! , only: [:dashboard]
+
+	def home
+		if current_user
+			redirect_to dashboard_path
+		end
+	end
+
+	def dashboard
+		@user = current_user
+		@places = @user.places
+		@reviews = @user.reviews
+	end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,12 @@ class Review < ActiveRecord::Base
 	belongs_to :place
 	validates_presence_of :content
 	validates_presence_of :score
+
+	#  create callback function
+	after_save :calculate_average
+
+	def calculate_average
+		a = self.place.average_rating
+		self.place.update_attributes(total_average_rating: a)
+	end
 end
